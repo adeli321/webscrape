df8ce03c5db29aa34d82389125a706cf462ae2b8
Anthony Delivanis <anthony.delivanis@gmail.com>
2019-04-25 20:34:22 +0100

    added function

--- indeed_uk.py
+++ indeed_uk.py
@@ -15,29 +15,30 @@ url       = ['https://www.indeed.co.uk/jobs?q=junior+data+engineer&l=',
             'https://www.indeed.co.uk/jobs?q=junior+data+engineer&start=20', 
             'https://www.indeed.co.uk/jobs?q=junior+data+engineer&start=30']
 
-for i in url:
-    timestamp = datetime.datetime.now()
-    page      = requests.get(i)
-    soup      = BeautifulSoup(page.text, 'lxml')
+def scrape_data(url_list):
+    for i in url:
+        timestamp = datetime.datetime.now()
+        page      = requests.get(i)
+        soup      = BeautifulSoup(page.text, 'lxml')
+        job_names = soup.find_all('a', class_='jobtitle')
 
-    job_names = soup.find_all('a', class_='jobtitle')
+        job_links = []
+        for i in range(len(job_names)):
+            job_links.append(job_names[i]['href'])
 
-    job_links = []
-    for i in range(len(job_names)):
-        job_links.append(job_names[i]['href'])
+        with UsePostgres(db_config) as cursor:
+            for i in job_links:
+                new_job  = requests.get('https://www.indeed.co.uk' + i)
+                new_soup = BeautifulSoup(new_job.text, 'lxml')
+                summary  = new_soup.find('div', id='jobDescriptionText').text
+                title    = new_soup.find('h3').text
+                comp_loc = new_soup.find('div', 'jobsearch-InlineCompanyRating').text
+                split    = comp_loc.split('-')
+                company  = split[0]
+                location = split[1]
+                cursor.execute("""INSERT INTO job_listings(title, company, location, summary, timestamp) 
+                                VALUES(%s, %s, %s, %s, %s)""", 
+                                (title, company, location, summary, timestamp))
 
-    with UsePostgres(db_config) as cursor:
-        for i in job_links:
-            new_job  = requests.get('https://www.indeed.co.uk' + i)
-            new_soup = BeautifulSoup(new_job.text, 'lxml')
-            summary  = new_soup.find('div', id='jobDescriptionText').text
-            title    = new_soup.find('h3').text
-            comp_loc = new_soup.find('div', 'jobsearch-InlineCompanyRating').text
-            split    = comp_loc.split('-')
-            company  = split[0]
-            location = split[1]
-            cursor.execute("""INSERT INTO job_listings(title, company, location, summary, timestamp) 
-                            VALUES(%s, %s, %s, %s, %s)""", 
-                            (title, company, location, summary, timestamp))
-
-
+if __name__ == '__main__':
+    scrape_data(url)
